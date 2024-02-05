f46ab3aa1b6804df0eb0c1f38e2816092ca46c00
kimmaloneintel <kim.malone@intel.com>
2024-02-05 10:46:17 -0800

    Tweaks to torchlens_exp.py
    - Updated to use the prompt argument instead of hardcoding it
    - Updated to use model argument instead of hardcoding it
    - Changed the model filename to minigpt.pth instead of minigpt-cpu.pth, which didn't exist
    
    - Changed torchlens log_forward_pass call to generate unrolled graph (was none)
    - Updated graph.gv.pdf based on changes to torchlens
    - Added log.txt that corresponds to graph.gv.pdf

--- torchlens_exp.py
+++ torchlens_exp.py
@@ -10,7 +10,7 @@ parser.add_argument('-m', '--model', default='minigpt.pth', help='Input text for
 parser.add_argument('-l', '--gen_length', default=100, help='Maximum length to generate.', type=int)
 parser.add_argument('--c2i', default='c2i.json', help='Token map file (character to index).', type=str)
 parser.add_argument('--i2c', default='i2c.json', help='Token map file (index to character).', type=str)
-parser.add_argument('--prompt', default=' ', help='Prompt for text generation.', type=str)
+parser.add_argument('--prompt', default='dummy input', help='Prompt for text generation.', type=str)
 parser.add_argument('--webui', action='store_true', help='If specified, use streamlit-based Web UI instead of command line.')
 
 args = parser.parse_args()
@@ -24,15 +24,16 @@ print(f"Using {device} device")
 
 c2i, i2c = load_token_map(c2i_file=args.c2i, i2c_file=args.i2c)
 
-my_model = torch.load('minigpt-cpu.pth')
+print(f"Loading model {args.model}")
+my_model = torch.load(args.model)
 my_model.eval()
 
-dummy_input = torch.tensor(encoder("dummy input", c2i), device=device).unsqueeze(0)
+print(f"Using prompt {args.prompt}")
+model_input = torch.tensor(encoder(args.prompt, c2i), device=device).unsqueeze(0)
 
-print(dummy_input)
+print(model_input)
 
-#model_history = tl.log_forward_pass(my_model, dummy_input, layers_to_save='all', vis_opt='unrolled')
-model_history = tl.log_forward_pass(my_model, dummy_input, layers_to_save='all', vis_opt='none')
+model_history = tl.log_forward_pass(my_model, model_input, layers_to_save='all', vis_opt='unrolled')
 
 logFile = open('log.txt', 'w')
 print(model_history, file = logFile)
